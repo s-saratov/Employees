d5bd0c9cd52a46a16c88f968e94eff3385f7e449
Anastasiia Yemets <anastasiaemec40@gmail.com>
2025-02-06 11:11:13 +0100

    job position changes

--- src/Pages/EmployeeProjectForm/EmployeeProjectForm.tsx
+++ src/Pages/EmployeeProjectForm/EmployeeProjectForm.tsx
@@ -31,7 +31,8 @@ function EmployeeProjectForm() {
       .required("Field <age> is requaired")
       .min(18, "Age must be at least 18 years old")
       .max(80, "Age must be at most 80 years old"),
-    jobPosition: Yup.string().required().max(30, "Max 30 symbols"),
+    jobPosition: Yup.string()
+    .max(30, "Max 30 symbols"),
   });
 
   const formik = useFormik({
