f032ae5c26f36aabe3cbfed5b6c336b956e1798e
Anastasiia Yemets <anastasiaemec40@gmail.com>
2025-02-06 11:13:15 +0100

    form changes

--- src/Pages/EmployeeProjectForm/EmployeeProjectForm.tsx
+++ src/Pages/EmployeeProjectForm/EmployeeProjectForm.tsx
@@ -11,11 +11,7 @@ const employeeFormFields = [
   { name: "name", label: "Name*", placeholder: "Enter your name" },
   { name: "surname", label: "Surname*", placeholder: "Enter your surname" },
   { name: "age", label: "Age*", placeholder: "Enter your age" },
-  {
-    name: "jobPosition",
-    label: "Job position*",
-    placeholder: "Enter your job position",
-  },
+  { name: "jobPosition", label: "Job position", placeholder: "Enter your job position"},
 ];
 
 function EmployeeProjectForm() {
