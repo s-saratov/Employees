9ba84a5a5fcd3b0abae040ebed10b56d64877931
BogdanF <sovtour@gmail.com>
2025-02-07 17:33:52 +0100

    forma2

--- src/Pages/EmployeeCard/EmployeeCard.tsx
+++ src/Pages/EmployeeCard/EmployeeCard.tsx
@@ -1,4 +1,37 @@
-function EmployeeCard (){
-return("EmployeeCard")
+import { CardContainer, CardContent, InfoRow, Label, Value, Message } from './styles';
+import { useEmployeeContext } from '../EmployeeContext/EmployeeContext';
+
+function EmployeeCard() {
+  const { employee } = useEmployeeContext();
+
+  if (!employee) {
+    return <Message>No employee data available. Please create an employee first.</Message>;
+  }
+
+  return (
+    <CardContainer>
+      <CardContent>
+        <InfoRow>
+          <Label>Name:</Label>
+          <Value>{employee.name}</Value>
+        </InfoRow>
+        <InfoRow>
+          <Label>Surname:</Label>
+          <Value>{employee.surname}</Value>
+        </InfoRow>
+        {employee.age && (
+          <InfoRow>
+            <Label>Age:</Label>
+            <Value>{employee.age}</Value>
+          </InfoRow>
+        )}
+        <InfoRow>
+          <Label>Job Position:</Label>
+          <Value>{employee.jobPosition}</Value>
+        </InfoRow>
+      </CardContent>
+    </CardContainer>
+  );
 }
+
 export default EmployeeCard;
